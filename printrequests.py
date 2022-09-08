9d1e672297f9b1fb4f149406dcea5c6ef2fa19d0
mharis2 <mharis2@ualberta.ca>
2022-09-08 16:45:44 -0600

    implemented last part of lab

--- printrequests.py
+++ printrequests.py
@@ -1,3 +1,9 @@
 import requests
+
 print(requests.__version__)
 print(requests.get('http://www.google.com'))
+req = requests.get('https://raw.githubusercontent.com/mharis2/cmput404lab1/master/printrequests.py','html.parser')
+print(req)
+with open("source_code",'w') as f:
+    f.write(req.text)
+    f.close()
